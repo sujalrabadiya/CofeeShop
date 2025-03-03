c4e49641b569d51557cd5899d9484cd04e5eb41e
Sujal Rabadiya <sujalrabadiya0@gmail.com>
2025-03-03 09:17:30 +0530

    Add files via upload

---
+++ WebApp (Mvc)/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CofeeShop.TagHelpers
+{
+    [HtmlTargetElement("button-submit")]
+    public class ButtonTagHelper : TagHelper
+    {
+        public string Text { get; set; } = "submit";
+        public string Class { get; set; } = "btn-primary";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+
+            output.Attributes.SetAttribute("type", Text);
+            output.Attributes.SetAttribute("class", $"btn btn-{Class}");
+            output.Content.SetContent(Text);
+        }
+    }
+}
---
+++ WebApp (Mvc)/TagHelpers/MyCustomTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CofeeShop.TagHelpers
+{
+    [HtmlTargetElement("sujal")]
+    public class MyCustomTagHelper : TagHelper
+    {
+        public string path { get; set; }
+        public string altText { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+            output.TagMode = TagMode.SelfClosing;
+
+            output.Attributes.SetAttribute("src", path);
+            output.Attributes.SetAttribute("alt", altText);
+        }
+    }
+}
